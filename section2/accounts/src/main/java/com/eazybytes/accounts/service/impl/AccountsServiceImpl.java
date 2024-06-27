846d8ad84986b8edfc7dc6c7409c53791b2f1d02
Simoo92 <simonesudosi@yahoo.it>
2024-06-27 17:45:35 +0200

    section 2 - lezione 23

--- section2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -13,10 +13,8 @@ import com.eazybytes.accounts.repository.AccountsRepository;
 import com.eazybytes.accounts.repository.CustomerRepository;
 import com.eazybytes.accounts.service.IAccountsService;
 import lombok.AllArgsConstructor;
-import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Service;
 
-import java.lang.module.ResolutionException;
 import java.time.LocalDateTime;
 import java.util.Optional;
 import java.util.Random;
@@ -39,8 +37,8 @@ public class AccountsServiceImpl implements IAccountsService {
             throw new CustomerAlreadyExistsException("Customer already registered with given mobileNumber"
                     +customerDto.getMobileNumber());
         }
-        customer.setCreatedAt(LocalDateTime.now());
-        customer.setCreatedBy("Anonymous");
+//        customer.setCreatedAt(LocalDateTime.now());
+//        customer.setCreatedBy("Anonymous");
         Customer savedCustomer = customerRepository.save(customer);
         accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -53,8 +51,8 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber((randomAccNumber));
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress((AccountsConstants.ADDRESS));
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
+//        newAccount.setCreatedAt(LocalDateTime.now());
+//        newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 
