808cbf3e0b6291977e3bc51c796443d548899f5b
Simoo92 <simonesudosi@yahoo.it>
2024-07-04 17:28:39 +0200

    section 2 - lezione 24,25,26,27,28,28,30

--- section2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ section2/accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,10 +1,15 @@
 package com.eazybytes.accounts.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
+@Schema(
+        name = "Account",
+        description = "Schema to hold Account information"
+)
 public class AccountsDto {
     @NotEmpty(message = "AccountNumber can not be a null or empty")
     @Pattern(regexp = "(^$|[0-9]{10})", message = "AccountNumber must be 10 digits")
